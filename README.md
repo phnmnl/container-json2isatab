761292bca226a938d0937b554176b9d6a58a61ad
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-02-15 15:35:28 +0000

    Updates for release

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ![Logo](isa-api_logo.png)
 
 # json2isatab: ISA-JSON to ISA-Tab Converter
-Version: 1.0.0
+Version: 0.5.0
 
 ## Short Description
 
--- run_json2tab.py
+++ run_json2tab.py
@@ -6,5 +6,9 @@ try:
     from isatools.convert import json2isatab
 except ImportError as e:
     raise RuntimeError("Could not import isatools package")
+if not os.path.exists(src_json):
+    print("File path to ISA-JSON file \"{}\" does not exist".format(src_json))
+    sys.exit(0)
+
 with open(src_json) as in_fp:
     json2isatab.convert(in_fp, target_dir)
