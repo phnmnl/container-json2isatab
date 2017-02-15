7df5fda8b350a289ddb5efbb7eb024bf78c34a7f
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-02-15 15:42:48 +0000

    update test data location

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update && apt-get install -y --no-install-recommends wget
-wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/json/BII-S-3/BII-S-3.json
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/json/BII-S-3/BII-S-3.json
 
 run_json2tab.py ./BII-S-3.json /tmp/
 
