73841f24993e94c7cec7f6b8c1680877f9c63e59
David Johnson <djcomlab@gmail.com>
2018-01-19 16:45:31 +0000

    Add openssl

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apk update && apk add ca-certificates && update-ca-certificates
+apk update && apk add ca-certificates && update-ca-certificates && apk add openssl
 wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/json/BII-S-3/BII-S-3.json
 
-run_json2tab.py BII-S-3.json
+run_json2tab.py ./BII-S-3.json
 
 # check that files were created
 if ! [ -e "i_investigation.txt" ]; then
